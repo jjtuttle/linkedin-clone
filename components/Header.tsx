7adaf29c5bfcf9d33cfccab6b374c6dea563bc7b
James Tuttle <james.j.tuttle@gmail.com>
2024-04-24 19:05:41 -0700

    saving work:added Header component, icons, &search icon.

---
+++ components/Header.tsx
@@ -0,0 +1,52 @@
+import { Briefcase, HomeIcon, Link, MessageSquare, SearchIcon, UsersIcon } from "lucide-react"
+import Image from "next/image"
+
+
+function Header() {
+    return (
+        <div className="flex items-center p-2 max-w-6xl mx-auto">
+            <Image 
+                className="rounded-lg"
+                src="https://links.papareact.com/b3z"
+                width={40}
+                height={40}
+                alt="logo"
+            />
+            <div className="flex-1">
+                <form className="flex items-center space-x-1 bg-gray-100 p-2 rounded-md flex-1 mx-2 max-w-96">
+                    <SearchIcon className="h-4 text-gray-600" />
+                    <input
+
+                        className="bg-transparent flex-1 outline-none"
+                        type="text"
+                        placeholder="Search" 
+                    />
+                </form>
+            </div>
+            <div className="">
+                <Link href="/" className="icon">
+                    <HomeIcon className="h-5" />
+                    <p>Home</p>
+                </Link>
+
+                <Link href="/" className="icon hidden md:flex">
+                    <UsersIcon className="h-5" />
+                    <p>Network</p>
+                </Link>
+
+                <Link href="/" className="icon hidden md:flex">
+                    <Briefcase className="h-5" />
+                    <p>Jobs</p>
+                </Link>
+
+                <Link href="/" className="icon">
+                    <MessageSquare className="h-5" />
+                    <p>Messaging</p>
+                </Link>
+
+            </div>
+        </div>
+    )
+}
+
+export default Header
